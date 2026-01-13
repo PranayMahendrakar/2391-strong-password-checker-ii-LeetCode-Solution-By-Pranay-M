b9e123036b5fe423b0057994bd51142a08a72048
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-13 16:16:32 +0530

    Initial commit

---
+++ 2025-12-29 14.46.37 - Accepted - runtime 0ms - memory 17.2MB.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        if len(password) < 8:
+            return False
+        
+        has_lower = any(c.islower() for c in password)
+        has_upper = any(c.isupper() for c in password)
+        has_digit = any(c.isdigit() for c in password)
+        has_special = any(c in "!@#$%^&*()-+" for c in password)
+        
+        # Check for adjacent same characters
+        has_adjacent = any(password[i] == password[i+1] for i in range(len(password)-1))
+        
+        return has_lower and has_upper and has_digit and has_special and not has_adjacent
